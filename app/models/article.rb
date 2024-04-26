cb2cc3cad097ca5da42d0ecf8d1b87328c3c1f4c
Victor Madera <v.maderaalbors@gmail.com>
2024-04-26 17:29:28 +0100

    implement validation and created a migration to adjust the DB to the validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ApplicationRecord
+    validates :title, presence: true, length: { maximum: 40 }
+    validates :body, presence: true, length: { maximum: 1000, too_long: "%{count} characters is the maximum allowed" }
+    
+    
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_25_183701) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_26_115336) do
   create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
+    t.string "title", limit: 40
+    t.text "body", limit: 1000
     t.boolean "published"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
